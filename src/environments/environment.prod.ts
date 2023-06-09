2b4809a5f63de5a6413a68ce4d9c56e9c2f21fb8
ToutsopDongmoJordan <jordantoutsop472@yahoo.fr>
2023-06-09 23:15:54 +0100

    last commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
   production: true,
    firebaseConfig : {
-    apiKey: "AIzaSyDpbQUpTCRXIIUU-JKVteJfGc_m7PoT248",
-    authDomain: "online-shopping-f476e.firebaseapp.com",
-    projectId: "online-shopping-f476e",
-    storageBucket: "online-shopping-f476e.appspot.com",
-    messagingSenderId: "668702531263",
-    appId: "1:668702531263:web:f2c983082d2a77b10f700a",
-    measurementId: "G-ZFQHH6ZE6W"
+    apiKey: "AIzaSyCQs8Gkfa7aMcwknkZdXw3aft1uQUsbBxo",
+    authDomain: "online-shopping-f9a2a.firebaseapp.com",
+    projectId: "online-shopping-f9a2a",
+    storageBucket: "online-shopping-f9a2a.appspot.com",
+    messagingSenderId: "439264993666",
+    appId: "1:439264993666:web:a5218651fdc9ec2fc7ef9b",
+    measurementId: "G-3B8PG0NPV8"
   }
 };
